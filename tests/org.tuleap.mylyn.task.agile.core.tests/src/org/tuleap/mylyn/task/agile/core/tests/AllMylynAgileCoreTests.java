d13f34737abe9f92ecbd19962837ffb6091817a4
Laurent Delaigue <laurent.delaigue@obeo.fr>
2013-09-23 11:34:15 +0200

    Removed useless old classes.

--- tests/org.tuleap.mylyn.task.agile.core.tests/src/org/tuleap/mylyn/task/agile/core/tests/AllMylynAgileCoreTests.java
+++ tests/org.tuleap.mylyn.task.agile.core.tests/src/org/tuleap/mylyn/task/agile/core/tests/AllMylynAgileCoreTests.java
@@ -16,7 +16,6 @@ import junit.textui.TestRunner;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.tuleap.mylyn.task.internal.agile.core.tests.TaskAttributeWrapperTest;
 import org.tuleap.mylyn.task.internal.agile.core.tests.data.CardwallTaskMapperTests;
 import org.tuleap.mylyn.task.internal.agile.core.tests.data.MilestonePlanningWrapperTest;
 
@@ -27,8 +26,7 @@ import org.tuleap.mylyn.task.internal.agile.core.tests.data.MilestonePlanningWra
  * @since 1.0
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TaskAttributeWrapperTest.class, CardwallTaskMapperTests.class,
-		MilestonePlanningWrapperTest.class })
+@Suite.SuiteClasses({CardwallTaskMapperTests.class, MilestonePlanningWrapperTest.class })
 public final class AllMylynAgileCoreTests {
 
 	/**
