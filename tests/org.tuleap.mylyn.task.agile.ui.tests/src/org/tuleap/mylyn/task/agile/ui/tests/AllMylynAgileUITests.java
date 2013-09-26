cb2663f75d0b0089cd72323d644db5b282d05695
Cedric Notot <cedric.notot@obeo.fr>
2013-09-26 17:49:29 +0200

    Add tests for Mylyn task Agile UI.

---
+++ tests/org.tuleap.mylyn.task.agile.ui.tests/src/org/tuleap/mylyn/task/agile/ui/tests/AllMylynAgileUITests.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.tuleap.mylyn.task.agile.ui.tests;
+
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.Test;
+import junit.textui.TestRunner;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.tuleap.mylyn.task.agile.ui.tests.data.BacklogModelTest;
+
+/**
+ * This class should be used to launch all the unit tests.
+ * 
+ * @author <a href="mailto:cedric.notot@obeo.fr">Cedric Notot</a>
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BacklogModelTest.class })
+public final class AllMylynAgileUITests {
+
+	/**
+	 * The constructor.
+	 */
+	private AllMylynAgileUITests() {
+		// prevent instantiation
+	}
+
+	/**
+	 * Launches the test with the given arguments.
+	 * 
+	 * @param args
+	 *            Arguments of the testCase.
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * Creates the {@link junit.framework.TestSuite TestSuite} for all the test.
+	 * 
+	 * @return The test suite containing all the tests
+	 */
+	public static Test suite() {
+		return new JUnit4TestAdapter(AllMylynAgileUITests.class);
+	}
+}
---
+++ tests/org.tuleap.mylyn.task.agile.ui.tests/src/org/tuleap/mylyn/task/agile/ui/tests/data/BacklogModelTest.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.tuleap.mylyn.task.agile.ui.tests.data;
+
+import java.util.Date;
+
+import org.eclipse.mylyn.tasks.core.TaskRepository;
+import org.eclipse.mylyn.tasks.core.data.TaskAttributeMapper;
+import org.eclipse.mylyn.tasks.core.data.TaskData;
+import org.junit.Before;
+import org.junit.Test;
+import org.tuleap.mylyn.task.agile.core.data.planning.BacklogItemWrapper;
+import org.tuleap.mylyn.task.agile.core.data.planning.MilestonePlanningWrapper;
+import org.tuleap.mylyn.task.agile.core.data.planning.SubMilestoneWrapper;
+import org.tuleap.mylyn.task.internal.agile.ui.editors.planning.MilestoneBacklogModel;
+import org.tuleap.mylyn.task.internal.agile.ui.editors.planning.SubMilestoneBacklogModel;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests of the Backlog model classes.
+ * 
+ * @author <a href="mailto:cedric.notot@obeo.fr">Cedric Notot</a>
+ */
+public class BacklogModelTest {
+
+	/**
+	 * The wrapped task data.
+	 */
+	private TaskData taskData;
+
+	/**
+	 * test SubMilestoneBacklogModel.
+	 */
+	@Test
+	public void testMilestoneBacklogModel() {
+		MilestonePlanningWrapper wrapper = new MilestonePlanningWrapper(taskData.getRoot());
+
+		SubMilestoneWrapper subMilestone = wrapper.addSubMilestone(200);
+		subMilestone.setCapacity(20);
+		subMilestone.setDuration(11);
+		subMilestone.setLabel("Milestone 1"); //$NON-NLS-1$
+		subMilestone.setStartDate(new Date());
+
+		BacklogItemWrapper backlogItem = wrapper.addBacklogItem(300);
+		backlogItem.setAssignedMilestoneId(200);
+		backlogItem.setInitialEffort(5);
+		backlogItem.setLabel("backlog item 1"); //$NON-NLS-1$
+
+		BacklogItemWrapper backlogItem2 = wrapper.addBacklogItem(301);
+		backlogItem2.setInitialEffort(10);
+		backlogItem2.setLabel("backlog item 2"); //$NON-NLS-1$
+
+		MilestoneBacklogModel backlogModel = new MilestoneBacklogModel(wrapper);
+
+		// CHECKING
+		assertEquals(backlogModel.getMilestoneId(), null);
+		assertEquals(backlogModel.getBacklogItems().size(), 1);
+		assertEquals(backlogModel.getBacklogItems().get(0).getId(), backlogItem2.getId());
+		assertEquals(backlogModel.getMilestonePlanning(), wrapper);
+	}
+
+	/**
+	 * test SubMilestoneBacklogModel.
+	 */
+	@Test
+	public void testSubMilestoneBacklogModel() {
+		MilestonePlanningWrapper wrapper = new MilestonePlanningWrapper(taskData.getRoot());
+
+		SubMilestoneWrapper subMilestone = wrapper.addSubMilestone(200);
+		subMilestone.setCapacity(20);
+		subMilestone.setDuration(11);
+		subMilestone.setLabel("Milestone 1"); //$NON-NLS-1$
+		subMilestone.setStartDate(new Date());
+		SubMilestoneBacklogModel backlogModel = new SubMilestoneBacklogModel(wrapper, subMilestone);
+
+		BacklogItemWrapper backlogItem = wrapper.addBacklogItem(300);
+		backlogItem.setInitialEffort(5);
+		backlogItem.setLabel("backlog item 1"); //$NON-NLS-1$
+
+		BacklogItemWrapper backlogItem2 = wrapper.addBacklogItem(301);
+		backlogItem2.setAssignedMilestoneId(200);
+		backlogItem2.setInitialEffort(5);
+		backlogItem2.setLabel("backlog item 2"); //$NON-NLS-1$
+
+		BacklogItemWrapper backlogItem3 = wrapper.addBacklogItem(302);
+		backlogItem3.setAssignedMilestoneId(201);
+		backlogItem3.setInitialEffort(5);
+		backlogItem3.setLabel("backlog item 3"); //$NON-NLS-1$
+
+		// CHECKING
+		assertEquals(backlogModel.getMilestoneId().intValue(), subMilestone.getId());
+		assertEquals(backlogModel.getSubMilestone(), subMilestone);
+		assertEquals(backlogModel.getBacklogItems().size(), 1);
+		assertEquals(backlogModel.getBacklogItems().get(0).getId(), backlogItem2.getId());
+		assertEquals(backlogModel.getMilestonePlanning(), wrapper);
+	}
+
+	/**
+	 * Configure the data for the tests.
+	 */
+	@Before
+	public void setUp() {
+		String repositoryUrl = "repository"; //$NON-NLS-1$
+		String connectorKind = "kind"; //$NON-NLS-1$
+		String taskId = "id"; //$NON-NLS-1$ 
+		TaskRepository taskRepository = new TaskRepository(connectorKind, repositoryUrl);
+		TaskAttributeMapper mapper = new TaskAttributeMapper(taskRepository);
+		taskData = new TaskData(mapper, connectorKind, repositoryUrl, taskId);
+	}
+
+}
