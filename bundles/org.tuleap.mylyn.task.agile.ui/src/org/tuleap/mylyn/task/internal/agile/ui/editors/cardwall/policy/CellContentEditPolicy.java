2feb26ad890d23e10d358d57b35f344498889811
Laurent Delaigue <laurent.delaigue@obeo.fr>
2013-10-31 14:39:37 +0100

    Cards are not resizable.

--- bundles/org.tuleap.mylyn.task.agile.ui/src/org/tuleap/mylyn/task/internal/agile/ui/editors/cardwall/policy/CellContentEditPolicy.java
+++ bundles/org.tuleap.mylyn.task.agile.ui/src/org/tuleap/mylyn/task/internal/agile/ui/editors/cardwall/policy/CellContentEditPolicy.java
@@ -13,8 +13,10 @@ package org.tuleap.mylyn.task.internal.agile.ui.editors.cardwall.policy;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.EditPart;
+import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editpolicies.ConstrainedLayoutEditPolicy;
+import org.eclipse.gef.editpolicies.NonResizableEditPolicy;
 import org.eclipse.gef.requests.ChangeBoundsRequest;
 import org.eclipse.gef.requests.CreateRequest;
 import org.tuleap.mylyn.task.internal.agile.ui.editors.cardwall.command.ChangeCardStatusCommand;
@@ -74,4 +76,13 @@ public class CellContentEditPolicy extends ConstrainedLayoutEditPolicy {
 		return null;
 	}
 
+	/**
+	 * Use appropriate edit policy for children cards, that are not resizable. {@inheritDoc}
+	 * 
+	 * @see org.eclipse.gef.editpolicies.ConstrainedLayoutEditPolicy#createChildEditPolicy(org.eclipse.gef.EditPart)
+	 */
+	@Override
+	protected EditPolicy createChildEditPolicy(EditPart child) {
+		return new NonResizableEditPolicy();
+	}
 }
